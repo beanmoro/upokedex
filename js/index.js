776aed97c1039e243ab2237e7dab66f81d984f2b
Benjamin Moraga R <bean.moro@gmail.com>
2021-04-15 17:12:17 -0400

    Agregar Contenido en las cards y JS

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+
+    let nombre = document.querySelector("#nombre-txt").value;
+    alert("Hola mundo!" + nombre);
+    
+});
