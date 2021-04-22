549150470d082e094b4f173b62b2616785818487
Benjamin Moraga R <bean.moro@gmail.com>
2021-04-22 17:26:55 -0400

    Clase 22/04/21

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
+
+tinymce.init({
+    selector: '#descripcion-txt',
+    height: 500,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
 
     let nombre = document.querySelector("#nombre-txt").value;
-    alert("Hola mundo!" + nombre);
+    let descripcion = tinymce.get("descripcion-txt").getContent();
+    let legendario = document.querySelector("#legendario-si").checked;
+    let tipo = document.querySelector("#tipo-select").value;
+
+    console.log("Hola Mundo! ", nombre, descripcion, legendario, tipo);
     
 });
